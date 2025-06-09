30aff198206f3ae57d62b3e300b559c75e11e6b5
Omar Mattos <omarmatt84@gmail.com>
2025-06-09 20:17:29 -0300

    agregamos el componente información

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Informacion from './components/informacion';
+
 function App() {
-  return <h1>Hola Mundo</h1>;
+  //aqui va la logica del componente
+
+  return (
+  // aqui puedo agregar algo de logica
+    <>
+    {/* Aqui va todo el maquetado html*/}
+      <h1>Hola Mundo</h1>;
+      <Informacion></Informacion>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/informacion.jsx
@@ -0,0 +1,9 @@
+const Informacion = () => {
+    return (
+        <div>
+            <h2>Primer proyecto de react</h2>   
+        </div>
+    );
+};
+
+export default Informacion;
