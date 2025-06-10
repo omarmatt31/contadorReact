e9b1a982b67857a7b29cbb30a9e5df6b0b671f26
Omar Mattos <omarmatt84@gmail.com>
2025-06-09 21:31:51 -0300

    agregamos las funciones sumar y restar, además ejemplo de uso de estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Informacion from './components/informacion';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/informacion";
+import Contador from "./components/Contador";
 
 function App() {
   //aqui va la logica del componente
-  const comision = 'c11'
+  const comision = "c11";
 
   return (
-  // aqui puedo agregar algo de logica
+    // aqui puedo agregar algo de logica
     <>
-    {/* Aqui va todo el maquetado html*/}
-    <main className='container my-5'>
-      <h1>Hola Mundo</h1>
-      <Informacion comisionProps={comision}  anio={2025}/>
-    </main>
-
+      {/* Aqui va todo el maquetado html*/}
+      <main className="container my-5">
+        <h1>Hola Mundo</h1>
+        <Informacion comisionProps={comision} anio={2025} />
+        <Contador />
+      </main>
     </>
   );
 }
